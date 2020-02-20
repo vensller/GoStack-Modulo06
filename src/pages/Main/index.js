2d23a0c67358a45175aa2d3dad5f451d4832233e
Ivens Diego Müller <ivens.muller@amazingit.com.br>
2020-02-20 20:02:18 -0300

    Started Module06

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+// import { Container } from './styles';
+
+export default function Main() {
+  return <Container />;
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};

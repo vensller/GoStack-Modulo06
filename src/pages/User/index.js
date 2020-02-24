314c69c13a3e9a6709de15a0cfd12418dfb66eb2
Ivens Diego Müller <ivens.muller@amazingit.com.br>
2020-02-24 19:15:19 -0300

    Finishing Desafio 06

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
@@ -24,11 +25,16 @@ export default class User extends Component {
   static propTypes = {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
+      navigate: PropTypes.func,
     }).isRequired,
   };
 
   state = {
     stars: [],
+    loading: true,
+    loadingMore: false,
+    refreshing: false,
+    page: 1,
   };
 
   async componentDidMount() {
@@ -37,12 +43,59 @@ export default class User extends Component {
 
     const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({ stars: response.data });
+    this.setState({ stars: response.data, loading: false });
   }
+
+  loadMoreStars = async () => {
+    const { stars, page } = this.state;
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    if (stars.length === page * 30) {
+      this.setState({ loadingMore: true });
+
+      const response = await api.get(`/users/${user.login}/starred`, {
+        params: {
+          page: page + 1,
+        },
+      });
+
+      this.setState({
+        stars: [...stars, ...response.data],
+        page: page + 1,
+        loadingMore: false,
+      });
+    }
+  };
+
+  refreshList = async () => {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    this.setState({ refreshing: true });
+
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: {
+        page: 1,
+      },
+    });
+
+    this.setState({
+      stars: response.data,
+      page: 1,
+      refreshing: false,
+    });
+  };
+
+  handleNavigate = async repository => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Repository', { repository });
+  };
 
   render() {
     const { navigation } = this.props;
-    const { stars } = this.state;
+    const { stars, loading, loadingMore, refreshing } = this.state;
 
     const user = navigation.getParam('user');
 
@@ -54,11 +107,17 @@ export default class User extends Component {
           <Bio>{user.bio}</Bio>
         </Header>
 
+        {loading && <ActivityIndicator color="#7159c1" />}
+
         <Stars
           data={stars}
+          onEndReachedThreshold={0.2}
+          onEndReached={this.loadMoreStars}
+          onRefresh={this.refreshList}
+          refreshing={refreshing}
           keyExtractor={star => String(star.id)}
           renderItem={({ item }) => (
-            <Starred>
+            <Starred onPress={() => this.handleNavigate(item)}>
               <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
               <Info>
                 <Title>{item.name}</Title>
@@ -67,6 +126,8 @@ export default class User extends Component {
             </Starred>
           )}
         />
+
+        {loadingMore && <ActivityIndicator color="#7159c1" />}
       </Container>
     );
   }
